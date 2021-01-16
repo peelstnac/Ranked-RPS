ea1f2a5cd410bca5d090b490ad83fae1f8010fa8
peelstnac <chfrn2002@gmail.com>
2021-01-16 00:06:46 -0500

    refactor: added more boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const App = () => {
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <h1 onClick={() => {
+                fetch('/auth/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        credentials: {
+                            username: '1',
+                            password: '2'
+                        }
+                    })
+                });
+            }}>Hello World!</h1>
+            <h1 onClick={() => {
+                fetch('/auth/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        credentials: {
+                            username: '1',
+                            password: '2'
+                        }
+                    })
+                });
+            }}>Hello World!</h1>
+        </div>
     );
 };
 
