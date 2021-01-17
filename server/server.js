c5d92424084d3c61ab7dd27db22634385c9059a7
peelstnac <chfrn2002@gmail.com>
2021-01-17 15:24:48 -0500

    build: hackathon deploy patch

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use('/auth', auth);
 app.use('/lobby', lobby);
 app.use('/data', data);
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
